69345279b30600e5dc281ed650303cabe67d0f51
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-08-08 18:55:00 -0400

    Create the recycler view in the admin console and allow for approving or removing events - Winston Heinrichs

--- app/src/main/java/edu/northeastern/numad24su_group9/AdminConsole.java
+++ app/src/main/java/edu/northeastern/numad24su_group9/AdminConsole.java
@@ -1,14 +1,37 @@
 package edu.northeastern.numad24su_group9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.northeastern.numad24su_group9.firebase.repository.database.EventRepository;
+import edu.northeastern.numad24su_group9.model.Event;
+import edu.northeastern.numad24su_group9.recycler.AdminConsoleAdapter;
+import edu.northeastern.numad24su_group9.recycler.EventAdapter;
 
 public class AdminConsole extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private AdminConsoleAdapter adapter;
+    private List<Event> eventList;
+    private ArrayList<Event> allEvents;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +43,48 @@ public class AdminConsole extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        progressBar = findViewById(R.id.AdminConsoleProgressBar);
+        getEvents();
+    }
+
+    public void getEvents() {
+        allEvents = new ArrayList<>();
+        EventRepository eventRepository = new EventRepository();
+
+        Task<DataSnapshot> task = eventRepository.getEventRef().get();
+        task.addOnSuccessListener(dataSnapshot -> {
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot eventSnapshot : dataSnapshot.getChildren()) {
+                    if (Boolean.TRUE.equals(eventSnapshot.child("isReported").getValue(Boolean.class))) {
+                        Event event = new Event();
+                        event.setEventID(eventSnapshot.getKey());
+                        event.setTitle(eventSnapshot.child("title").getValue(String.class));
+                        event.setImage(eventSnapshot.child("image").getValue(String.class));
+                        event.setDescription(eventSnapshot.child("description").getValue(String.class));
+                        event.setStartTime(eventSnapshot.child("startTime").getValue(String.class));
+                        event.setStartDate(eventSnapshot.child("startDate").getValue(String.class));
+                        event.setEndTime(eventSnapshot.child("endTime").getValue(String.class));
+                        event.setEndDate(eventSnapshot.child("endDate").getValue(String.class));
+                        event.setPrice(eventSnapshot.child("price").getValue(String.class));
+                        event.setLocation(eventSnapshot.child("location").getValue(String.class));
+                        event.setRegisterLink(eventSnapshot.child("registerLink").getValue(String.class));
+                        event.setIsReported(eventSnapshot.child("isReported").getValue(Boolean.class));
+                        allEvents.add(event);
+                    }
+                }
+            }
+            updateUI(allEvents);
+        }
+        ).addOnFailureListener(Throwable::printStackTrace);
+    }
+
+    private void updateUI(List<Event> events) {
+        recyclerView = findViewById(R.id.adminConsoleRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new AdminConsoleAdapter(this);
+        adapter.updateData(events);
+        recyclerView.setAdapter(adapter);
+        progressBar.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/edu/northeastern/numad24su_group9/recycler/AdminConsoleAdapter.java
@@ -0,0 +1,110 @@
+package edu.northeastern.numad24su_group9.recycler;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import edu.northeastern.numad24su_group9.R;
+import edu.northeastern.numad24su_group9.firebase.repository.database.EventRepository;
+import edu.northeastern.numad24su_group9.firebase.repository.storage.EventImageRepository;
+import edu.northeastern.numad24su_group9.model.Event;
+
+public class AdminConsoleAdapter extends RecyclerView.Adapter<AdminConsoleAdapter.ViewHolder> {
+    private List<Event> events;
+    private EventAdapter.OnItemClickListener listener;
+    private EventAdapter.OnItemSelectListener selectListener;
+    private Context context;
+
+    public AdminConsoleAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void updateData(List<Event> events) {
+        this.events = events;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_admin_console_recycler_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Event event = events.get(position);
+
+        EventImageRepository eventImageRepository = new EventImageRepository();
+
+        Picasso.get().load(eventImageRepository.getEventImage(event.getImage())).into(holder.imageView);
+        holder.titleTextView.setText(event.getTitle());
+        holder.descriptionTextView.setText(event.getDescription());
+        holder.approveButton.setOnClickListener(v -> {
+            event.setIsReported(false);
+            EventRepository eventRepository = new EventRepository();
+            DatabaseReference eventRef = eventRepository.getEventRef().child(event.getEventID());
+            eventRef.setValue(event);
+            // Remove the event from the list
+            events.remove(position);
+            // Notify the adapter that the item has been removed
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, events.size());});
+
+        // Delete the event from the database
+        holder.rejectButton.setOnClickListener(v -> {
+            EventRepository eventRepository = new EventRepository();
+            DatabaseReference eventRef = eventRepository.getEventRef().child(event.getEventID());
+            eventRef.removeValue()
+                    .addOnSuccessListener(aVoid -> {
+                        // Handle success
+                        // Remove the event from the list
+                        events.remove(position);
+                        // Notify the adapter that the item has been removed
+                        notifyItemRemoved(position);
+                        notifyItemRangeChanged(position, events.size());
+                        Toast.makeText(context, "Event deleted successfully", Toast.LENGTH_LONG).show();
+                    })
+                    .addOnFailureListener(e -> {
+                        // Handle failure
+                        Toast.makeText(context, "Failed to delete event", Toast.LENGTH_LONG).show();
+                    });
+            ;         });
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView titleTextView;
+        public TextView descriptionTextView;
+        public Button approveButton;
+        public Button rejectButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.event_image);
+            titleTextView = itemView.findViewById(R.id.event_name);
+            descriptionTextView = itemView.findViewById(R.id.event_description);
+            approveButton = itemView.findViewById(R.id.approve_event_button);
+            rejectButton = itemView.findViewById(R.id.remove_event_button);
+        }
+    }
+}
