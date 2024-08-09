cdca322e288b66d0b610312097b1006bb7e52f6a
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-08-09 11:56:20 -0400

    Added scroll view dependency - Winston Heinrichs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.coordinatorlayout)
+    implementation(libs.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
