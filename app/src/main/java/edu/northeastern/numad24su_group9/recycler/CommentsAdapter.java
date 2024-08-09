bb679e8431442b5264ec10eb518031942add7c77
Agllai Papaj <156248149+agllaip@users.noreply.github.com>
2024-08-08 17:11:47 -0700

    Comments Section in Progress

---
+++ app/src/main/java/edu/northeastern/numad24su_group9/recycler/CommentsAdapter.java
@@ -0,0 +1,72 @@
+package edu.northeastern.numad24su_group9.recycler;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import edu.northeastern.numad24su_group9.R;
+import edu.northeastern.numad24su_group9.model.Comment;
+
+public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.CommentViewHolder> {
+
+    private final List<Comment> commentsList;
+    private final Context context;
+
+    public CommentsAdapter(List<Comment> commentsList, Context context) {
+        this.commentsList = commentsList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_comment, parent, false);
+        return new CommentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
+        Comment comment = commentsList.get(position);
+        holder.commentText.setText(comment.getCommentText());
+        holder.commenterName.setText(comment.getCommentId()); // Assuming you want to show the comment ID as the name
+        holder.commentTimestamp.setText(formatTimestamp(comment.getTimestamp()));
+
+        // If you have a way to load avatar images, use Picasso or Glide here
+        // For now, let's assume a placeholder image is set.
+        Picasso.get().load(R.drawable.ic_profile).into(holder.commentAvatar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return commentsList.size();
+    }
+
+    public static class CommentViewHolder extends RecyclerView.ViewHolder {
+        TextView commenterName, commentTimestamp, commentText;
+        ImageView commentAvatar;
+
+        public CommentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            commenterName = itemView.findViewById(R.id.commenter_name);
+            commentTimestamp = itemView.findViewById(R.id.comment_timestamp);
+            commentText = itemView.findViewById(R.id.comment_text);
+            commentAvatar = itemView.findViewById(R.id.comment_avatar);
+        }
+    }
+
+    private String formatTimestamp(long timestamp) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        return sdf.format(new Date(timestamp));
+    }
+}
